8993f2256a6485a0cfa02d83d8e1ddc1983606e6
Marius Horga <mhorga@gmail.com>
2015-01-18 18:54:24 +0200

    Initial Commit

---
+++ iAd demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAd demo
+//
+//  Created by Marius Horga on 1/18/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iAd demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iAd demo
+//
+//  Created by Marius Horga on 1/18/15.
+//  Copyright (c) 2015 Marius Horga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
